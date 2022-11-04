306d486073ba6ebe37f1b5851708d01ee014d740
gilrjunior <gilmarjunioreis@gmail.com>
2022-11-04 17:44:55 -0300

    Atualização

--- ProjetoFabrica/src/tela/Tela.java
+++ ProjetoFabrica/src/tela/Tela.java
@@ -4,11 +4,12 @@
  * and open the template in the editor.
  */
 package tela;
+import carro.Carro;
+import carroDAO.CarroDAO;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-/**
- *
- * @author IFTM
- */
 public class Tela extends javax.swing.JFrame {
 
     /**
@@ -27,21 +28,178 @@ public class Tela extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jButton3 = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jtb_tabelabanco = new javax.swing.JTable();
+        jlb_chassi = new javax.swing.JLabel();
+        jlb_ano = new javax.swing.JLabel();
+        jlb_modelo = new javax.swing.JLabel();
+        jlb_fabricante = new javax.swing.JLabel();
+        jlb_potencia = new javax.swing.JLabel();
+        jtf_chassi = new javax.swing.JTextField();
+        jtf_ano = new javax.swing.JTextField();
+        jtf_modelo = new javax.swing.JTextField();
+        jtf_fabricante = new javax.swing.JTextField();
+        jtf_potencia = new javax.swing.JTextField();
+        jbt_inserir = new javax.swing.JButton();
+        jbt_atualizar = new javax.swing.JButton();
+        jbt_excluir = new javax.swing.JButton();
+
+        jButton3.setText("jButton3");
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jtb_tabelabanco.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(jtb_tabelabanco);
+
+        jlb_chassi.setText("Chassi");
+
+        jlb_ano.setText("Ano");
+
+        jlb_modelo.setText("Modelo");
+
+        jlb_fabricante.setText("Fabricante");
+
+        jlb_potencia.setText("Potência");
+
+        jbt_inserir.setText("Inserir");
+        jbt_inserir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jbt_inserirActionPerformed(evt);
+            }
+        });
+
+        jbt_atualizar.setText("Atualizar");
+        jbt_atualizar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jbt_atualizarActionPerformed(evt);
+            }
+        });
+
+        jbt_excluir.setText("Excluir");
+        jbt_excluir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jbt_excluirActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(22, 22, 22)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jlb_chassi, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jlb_ano)
+                            .addComponent(jlb_modelo)
+                            .addComponent(jlb_fabricante)
+                            .addComponent(jlb_potencia))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jtf_ano)
+                            .addComponent(jtf_modelo)
+                            .addComponent(jtf_fabricante)
+                            .addComponent(jtf_potencia)
+                            .addComponent(jtf_chassi, javax.swing.GroupLayout.PREFERRED_SIZE, 284, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(50, 50, 50)
+                        .addComponent(jbt_inserir, javax.swing.GroupLayout.PREFERRED_SIZE, 145, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jbt_atualizar, javax.swing.GroupLayout.PREFERRED_SIZE, 145, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(124, 124, 124)
+                        .addComponent(jbt_excluir, javax.swing.GroupLayout.PREFERRED_SIZE, 145, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 375, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 196, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jlb_chassi, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jtf_chassi, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jlb_ano, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jtf_ano, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jlb_modelo, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jtf_modelo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jlb_fabricante, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jtf_fabricante, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jlb_potencia, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jtf_potencia, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jbt_inserir)
+                    .addComponent(jbt_atualizar))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jbt_excluir)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jbt_inserirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbt_inserirActionPerformed
+        
+        Carro carro =  new Carro(jtf_chassi.getText(), Integer.parseInt(jtf_ano.getText()), jtf_modelo.getText(), jtf_fabricante.getText(), Float.parseFloat(jtf_potencia.getText()));
+       
+        try {
+            CarroDAO carroADD = new CarroDAO();
+            
+            carroADD.insertCarro(carro);
+            
+        } catch (SQLException ex) {
+            System.out.println("\n Erro na inserção: "+ex.toString());
+        } catch (ClassNotFoundException ex) {
+            System.out.println("\n Erro na inserção: "+ex.toString());
+        }
+        
+    }//GEN-LAST:event_jbt_inserirActionPerformed
+
+    private void jbt_atualizarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbt_atualizarActionPerformed
+        
+        Carro carro =  new Carro(jtf_chassi.getText(), Integer.parseInt(jtf_ano.getText()), jtf_modelo.getText(), jtf_fabricante.getText(), Float.parseFloat(jtf_potencia.getText()));
+       
+        try {
+            CarroDAO carroADD = new CarroDAO();
+            
+            carroADD.updateCarro(carro);
+            
+        } catch (SQLException ex) {
+            System.out.println("\n Erro na inserção: "+ex.toString());
+        } catch (ClassNotFoundException ex) {
+            System.out.println("\n Erro na inserção: "+ex.toString());
+        }
+        
+    }//GEN-LAST:event_jbt_atualizarActionPerformed
+
+    private void jbt_excluirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbt_excluirActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jbt_excluirActionPerformed
 
     /**
      * @param args the command line arguments
@@ -79,5 +237,21 @@ public class Tela extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton3;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JButton jbt_atualizar;
+    private javax.swing.JButton jbt_excluir;
+    private javax.swing.JButton jbt_inserir;
+    private javax.swing.JLabel jlb_ano;
+    private javax.swing.JLabel jlb_chassi;
+    private javax.swing.JLabel jlb_fabricante;
+    private javax.swing.JLabel jlb_modelo;
+    private javax.swing.JLabel jlb_potencia;
+    private javax.swing.JTable jtb_tabelabanco;
+    private javax.swing.JTextField jtf_ano;
+    private javax.swing.JTextField jtf_chassi;
+    private javax.swing.JTextField jtf_fabricante;
+    private javax.swing.JTextField jtf_modelo;
+    private javax.swing.JTextField jtf_potencia;
     // End of variables declaration//GEN-END:variables
 }
